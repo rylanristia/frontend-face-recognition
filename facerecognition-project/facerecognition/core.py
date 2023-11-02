56700f69ba1c184fbc01150c5a5c8f6e3e85a643
Rylanristia <50695667+rylanristia@users.noreply.github.com>
2023-11-02 17:57:42 +0700

    unknown terus nih

--- facerecognition-project/facerecognition/core.py
+++ facerecognition-project/facerecognition/core.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from PIL import Image
 from datetime import datetime
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
 import requests as req
 import json
 import base64
@@ -11,6 +13,10 @@ import io
 import imageio
 import numpy as np
 import cv2
+import pickle
+import face_recognition
+
+FILE_STORE = "face_recognition_model.pkl"
 
 def auth(request):
 
@@ -39,11 +45,11 @@ def auth(request):
 
 def addemployee(request):
     data_to_pass = {
-            'xnip': '220031',
-            'xname': 'Rylanristia',
-            'xemail': 'rylanristia@gmail.com',
-            'xphone_number': '085781077948',
-            'xaddress': 'Lorem'
+            'xnip': request.POST.get('xnip'),
+            'xname': request.POST.get('xname'),
+            'xemail': request.POST.get('xemail'),
+            'xphone_number': request.POST.get('xphone_number'),
+            'xaddress': request.POST.get('xaddress')
         }
 
     # Build the query string from the data
@@ -74,21 +80,106 @@ def addproceed(request):
         'ximage': request.POST.get('image')
     }
 
-    dd(context)
+    image = context['ximage']
+
+    # Specify the directory where you want to save the images
+    save_directory = '../image_face/' + context['xname'] + '/'  # Use the absolute path to the directory
+    
+    # Ensure the directory exists, creating it if necessary
+    os.makedirs(save_directory, exist_ok=True)
+
+    # Generate a unique filename (you can use a timestamp, random string, etc.)
+    # You can choose any file extension (e.g., PNG)
+
+    # Decode the Base64 data and save it as an image file
+    #image_data = frame.encode('utf-8')  # Convert the string data back to bytes
+    content_type, image_data = image.split(';base64,')
+    image_format = content_type.split('/')[-1]
+    filename = f'face_person.{image_format}'
+    image_path = os.path.join(save_directory, filename)
+
+    with open(image_path, 'wb') as image_file:
+        image_file.write(base64.b64decode(image_data))
+
+
+    known_faces_dir = "../image_face"
+
+    # Training
+    all_images_train = []
+    all_names_train = []
+    for person_dir in os.listdir(known_faces_dir):
+        if os.path.isdir(os.path.join(known_faces_dir, person_dir)):
+            person_name = person_dir
+            for image_file in os.listdir(os.path.join(known_faces_dir, person_dir)):
+                image_path = os.path.join(known_faces_dir, person_dir, image_file)
+                all_images_train.append(image_path)
+                all_names_train.append(person_name)
+
+    train(all_images_train, all_names_train)
+
+    res = {
+        'message' : 'Successfuly added new employee!' 
+    }
+    
+    return JsonResponse(res)
+
+
+def test():
+    try:
+        with open(FILE_STORE, 'rb') as file:
+            model = pickle.load(file)
+    except Exception as e:
+        raise Exception("You have to train first before testing it!")
+
+    known_face_encodings = model['known_face_encodings']
+    known_face_names = model['known_face_names']
+    
+    guesses = 'unknown'
+    test_image = face_recognition.load_image_file('../image_face_test/face_test.jpeg')
+    face_locations = face_recognition.face_locations(test_image)
+    test_face_encodings = face_recognition.face_encodings(test_image, face_locations)
+    for test_face_encoding in test_face_encodings:
+        matches = face_recognition.compare_faces(known_face_encodings, test_face_encoding)
+        
+        name = 'Unknown'
+        
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
+        
+        guesses = name
+    
+    return guesses
+
+
+# photos 
+def train(photos: list, names: list) -> None:
+    try:
+        with open(FILE_STORE, 'rb') as file:
+            model = pickle.load(file)
+    except Exception as e:
+        model = {
+            'known_face_encodings' : [],
+            'known_face_names' : []
+        }
+    face_encodings = []
+
+
+    for path in photos:
+        image = face_recognition.load_image_file(path)
+        face_encoding = face_recognition.face_encodings(image)
+        face_encodings.append(face_encoding)
+
+    model['known_face_encodings'] += face_encodings
+    model['known_face_names'] += names
+
+    with open(FILE_STORE, 'wb') as file:
+        pickle.dump(model, file)
+
 
 def recognize(request):
     data = request.body
 
-    # data = json.loads(data.decode('utf-8'))
-
-    # # Access specific values from the parsed data
-    # frame = data.get('frame', None)
-
-    # res = {
-    #     'frame' : frame
-    # }
-
-    # return JsonResponse(res)
     if request.method == 'POST':
         # Get the image data from the POST request
         data = request.body
@@ -98,7 +189,7 @@ def recognize(request):
 
         try:
             # Specify the directory where you want to save the images
-            save_directory = '../image_face/'  # Use the absolute path to the directory
+            save_directory = '../image_face_test/'  # Use the absolute path to the directory
             
             # Ensure the directory exists, creating it if necessary
             os.makedirs(save_directory, exist_ok=True)
@@ -116,9 +207,18 @@ def recognize(request):
             with open(image_path, 'wb') as image_file:
                 image_file.write(base64.b64decode(image_data))
 
-            # Return a JSON response with the image path or any other data you want
-            response_data = {'message': 'Image saved successfully', 'image_path': image_path}
-            return JsonResponse(response_data)
+            # # Return a JSON response with the image path or any other data you want
+            # response_data = {'message': 'Image saved successfully', 'image_path': image_path}
+
+            res = test()
+
+            # dd(res)
+
+            context = {
+                'frame' : res
+            }
+
+            return JsonResponse(context)
 
         except Exception as e:
             response_data = {'error': str(e)}
